5c3ebf87c3320c196488b1ce8d35064e8e9b5dcd
isaiasrider <isaiasdelima1@gmail.com>
2021-02-13 09:51:47 -0300

    last commit before turns it into versioned modules

--- terrform_up_and_running/modules_example/modules/data-stores/relational_database/backend.tf
+++
@@ -1,9 +0,0 @@
-//terraform {
-//  backend "s3" {
-//    bucket = "testando-estado-remoto"
-//    key = "prod/data-stores/relational_database/terraform.tfstate"
-//    region = "us-east-2"
-//    dynamodb_table = "dynamo-testando-estado-remoto"
-//    encrypt = true
-//  }
-//}
--- terrform_up_and_running/modules_example/stage/services/webserver_cluster/main.tf
+++ terrform_up_and_running/modules_example/stage/services/webserver_cluster/main.tf
@@ -3,7 +3,7 @@ module "webserver_cluster" {
 
   cluster_name = "ChikenFolks"
   db_remote_state_bucket = "testando-estado-remoto"
-  db_remote_state_key = "teste_module_withoutbackend/data-stores/relational_database/terraform.tfstate"
+  db_remote_state_key = "stage/data-stores/relational_database/mysql/terraform.tfstate"
   min_size = 1
   max_size = 2
 
