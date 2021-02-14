095cb91a27fa8e2da8affd7fe71440dfe11daa99
isaiasrider <isaiasdelima1@gmail.com>
2021-02-14 01:30:53 -0300

    create iam users

---
+++ infrastructure/live/global/iam/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_iam_user" "example" {
+  for_each = toset(var.user_names)
+  name = each.value
+
+}
---
+++ infrastructure/live/global/iam/variables.tf
@@ -0,0 +1,4 @@
+variable "user_names" {
+  type = list(string)
+  default = ["neo","trinity","morpheus"]
+}
